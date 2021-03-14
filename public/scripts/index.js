c543d40fd3a32cba2cdd6179787261af5fcdc015
Broccolingual <63525348+broccolingual@users.noreply.github.com>
2021-03-14 12:02:35 +0900

    update: マップビューワーのドラッグ移動、座標表示の追加、一部スクリプトにコメントを追加

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -13,6 +13,7 @@ function scrollToTarget(id) {
 const mapViewer = document.getElementById('mapViewer');
 const mapImageLarge = document.getElementById('mapImageLarge');
 
+// Open Map Viewer
 var maps = ['woods', 'shoreline', 'customs', 'reserve', 'interchange', 'factory', 'lab', 'streetsoftarkov']
 maps.forEach(function(e, i) {
     openTargetMaps(e)
@@ -26,17 +27,77 @@ function openTargetMaps(mapName) {
     });
 }
 
+// Map Viewer close
 const close = document.getElementById('close');
 close.addEventListener('click', function() {
     mapViewer.classList.remove('inview');
+    mapImageLarge.style.position = 'static'
 });
 
+// Map Viewer reset position
+const reset = document.getElementById('reset');
+reset.addEventListener('click', function(){
+    mapImageLarge.style.position = 'static'
+});
+
+// Map Viewer zoom
 const zoom = document.getElementById('zoom');
 zoom.addEventListener('click', function(){
-    alert('実装してないよ')
+    alert('座標の差分の計算めんどくさいから実装してないよ。')
 });
 
+// Map Viewer shrink
 const shrink = document.getElementById('shrink');
 shrink.addEventListener('click', function(){
-    alert('実装してないよ')
+    alert('座標の差分の計算めんどくさいから実装してないよ。')
 });
+
+// Map Viewer Drag & Drop
+mapImageLarge.onmousedown = function(e) {
+    var imageContainer = document.getElementById('imageContainer');
+    var diffWidth = ((window.innerWidth - imageContainer.clientWidth) / 2) - 14;
+    var diffHeight = ((window.innerHeight - imageContainer.clientHeight) / 2) - 5;
+
+    var shiftX = e.clientX - mapImageLarge.getBoundingClientRect().left + diffWidth;
+    var shiftY = e.clientY - mapImageLarge.getBoundingClientRect().top + diffHeight;
+
+    mapImageLarge.style.cursor = 'grab';
+    mapImageLarge.style.position = 'absolute';
+
+    moveAt(e.clientX, e.clientY);
+
+    function moveAt(clientX, clientY) {
+        mapImageLarge.style.left = clientX - shiftX + 'px';
+        mapImageLarge.style.top = clientY - shiftY + 'px';
+    }
+    
+    var x = document.getElementById('x');
+    var y = document.getElementById('y');
+
+    function onMouseMove(e) {
+        moveAt(e.clientX, e.clientY);
+        x.innerHTML = `X: ${e.clientX}`;
+        y.innerHTML = `Y: ${e.clientY}`;
+    }
+    
+    mapImageLarge.onmouseup = function() {
+        document.removeEventListener('mousemove', onMouseMove);
+        mapImageLarge.onmouseup = null;
+        mapImageLarge.style.cursor = 'pointer';
+        x.innerHTML = `X: ?`;
+        y.innerHTML = `Y: ?`;
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+}
+
+mapImageLarge.onmouseup = function() {
+    mapImageLarge.onmouseup = null;
+    mapImageLarge.style.cursor = 'pointer';
+    x.innerHTML = `X: ?`;
+    y.innerHTML = `Y: ?`;
+}
+
+mapImageLarge.ondragstart = function() {
+    return false;
+}
